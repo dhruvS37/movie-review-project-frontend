b0e6311b2de872c72f676eb95f5807d3d4748961
k24 <k24kevalam@gmail.com>
2024-03-11 18:45:54 +0530

    error & alert messages

--- app/category/category.component.js
+++ app/category/category.component.js
@@ -3,20 +3,32 @@ angular.module('category')
     .component('categoryInsert',{
         templateUrl : './category/category-insert-form.tpl.html',
         controller : ['$scope','$rootScope','categoryServices',function categoryInsertController($scope,$rootScope,categoryServices){
+            $scope.alert = null
+            $scope.setErrorMessage = function (data) {
+                let alert = { message: '' }
+                if ('movieName' in data)
+                    alert.message += data.movieName + "\n"
+                if ('category' in data)
+                    alert.message += data.category + "\n"
+                if ('rating' in data)
+                    alert.message += data.rating + "\n"
+                if ('message' in data)
+                    alert.message += data.message
+    
+                $scope.alert = alert
+            }
+
             $scope.addCategory = function(){
                 categoryServices.addCategory({category : $scope.categoryInput})
                 .then(function (response) { 
-                    $rootScope.alert = {
-                        message : response.data.message,
-                        type : 'success'
-                    }
+                    $scope.setErrorMessage(response.data)
+                    $scope.alert.type = 'success'
+
                     $scope.categoryInput = ''
                 })
                 .catch(function(error){
-                    $rootScope.alert = {
-                        message : response.data.message,
-                        type : 'danger'
-                    } 
+                    $scope.setErrorMessage(error.data)
+                    $scope.alert.type = 'danger'
                 })
             }
         }]
